39332703707e6ab787b576065f5c796f0ae29448
iheb dridi <ihebdridi123456@gmail.com>
2025-09-25 15:28:12 +0100

    b

--- Build/build.loader.js
+++ Build/build.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 82792173,
+    wasmFileSize: 82791961,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
